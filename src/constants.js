30b78111b7d05487d69cafc63bd3b8965df63612
kohz87 <83744063+kohz87@users.noreply.github.com>
2026-09-01 14:29:50 +0000

    Harden Inventory Block state protocol for v0.2.1

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const EXTENSION_ID = 'inventory-block';
-export const VERSION = '0.2.0';
+export const VERSION = '0.2.1';
 export const STATE_VERSION = 2;
 
 export const META_KEY = 'inventoryBlockV2';
@@ -8,6 +8,8 @@ export const PROMPT_KEY = 'inventory-block-state';
 
 export const UPDATE_COMMENT_MARKER = 'INVENTORY_BLOCK_UPDATE';
 export const UPDATE_TAG = 'InventoryUpdate';
+export const SEED_TAG = 'Inventory';
+export const ROOT_CATEGORY = 'General';
 
 export const SOURCE = Object.freeze({
     INIT: 'init',
